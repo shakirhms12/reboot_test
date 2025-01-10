2e2a36f1427731237fb71a6fb516c110e7ef6e78
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-10 13:40:20 +0530

    v3

--- variable.tf
+++ variable.tf
@@ -1,17 +1,17 @@
 
-variable "access_key" {
-  description = "access key"
-  sensitive = true
-}
+# variable "access_key" {
+#   description = "access key"
+#   sensitive = true
+# }
 
-variable "secret_key" {
-  description = "secret key"
-  sensitive = true
-}
+# variable "secret_key" {
+#   description = "secret key"
+#   sensitive = true
+# }
 
-variable "region" {
-  default = "us-east-1"
-}
+# variable "region" {
+#   default = "us-east-1"
+# }
 
 variable "tag" {
   description = "Provide the tag value of vm want to restart"
