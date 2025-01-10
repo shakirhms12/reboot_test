6f5db521641c048866726f1d8a5118ff99039181
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-10 13:42:27 +0530

    v4

--- variable.tf
+++ variable.tf
@@ -9,9 +9,9 @@
 #   sensitive = true
 # }
 
-# variable "region" {
-#   default = "us-east-1"
-# }
+variable "region" {
+  default = "us-east-1"
+}
 
 variable "tag" {
   description = "Provide the tag value of vm want to restart"
