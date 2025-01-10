92ab036063fb00dd281b5a53dbbe7055d0754225
shakirhms12 <2020a1r019@mietjammu.in>
2025-01-10 13:44:12 +0530

    v5

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 data "aws_instance" "target_instance" {
   filter {
     name   = "tag:Name"
--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
 
-# variable "access_key" {
-#   description = "access key"
-#   sensitive = true
-# }
+variable "access_key" {
+  description = "access key"
+  sensitive = true
+}
 
-# variable "secret_key" {
-#   description = "secret key"
-#   sensitive = true
-# }
+variable "secret_key" {
+  description = "secret key"
+  sensitive = true
+}
 
 variable "region" {
   default = "us-east-1"
